b3e76c85da89102149497fafbe0445181e2dd28a
GreatShay <amanda.aguilarmiller@gmail.com>
2019-10-31 12:02:23 -0600

    initial commit

---
+++ src/util/theme.js
@@ -0,0 +1,50 @@
+export default {
+    palette: {
+      primary: {
+        light: "#e8f5e9",
+        main: "#4caf50",
+        dark: "#1b5e20",
+        contrastText: "#fff"
+      },
+      secondary: {
+        light: "#c8e6c9",
+        main: "#66bb6a",
+        dark: "#2e7d32",
+        contrastText: "#fff"
+      }
+    },
+  
+    spreadThis: {
+      typography: {
+        useNextVariants: true
+      },
+      form: {
+        textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    },
+    pageTitle1: {
+        color: "#454545",
+        fontWeight: "bold"
+    },
+    pageTitle: {
+        margin: '20px auto 20px auto'
+    },
+    textField: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20,
+        position: 'relative'
+    },
+    customError: {
+        color: 'red',
+        fontSize: '0.8rem',
+        marginTop: 10
+    },
+    progress : {
+        position: 'absolute'
+    }
+  }
+  }
